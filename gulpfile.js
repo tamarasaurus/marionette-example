103dd3a131fe24393ffc4c4a2890c782678c72c4
Tamara Chahine <chahine.tamara@abc.net.au>
2014-08-31 23:24:21 +1000

    Reorganised gulpfile and added compass

--- gulpfile.js
+++ gulpfile.js
@@ -10,83 +10,95 @@ var connect = require('gulp-connect');
 var rename = require('gulp-rename');
 var jshintOpts = require('./jshint');
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var compass = require('gulp-compass');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
 // Utility functions for build e.g. logging and error handling
 var utils = {
   error: function(e) {
     console.log(e);
   },
+  connect: function() {
+    connect.server({
+      root: './dist',
+      livereload: true,
+      port: 8888
+    });
+  },
+  hint: function() {
+    return gulp.src('./src/**/*.js')
+      .pipe(hint(jshintOpts))
+      .pipe(hint.reporter('default'));
+  },
   end: function() {
     console.log('Compiled!');
   }
 };
 
-// Compile the main index.hbs > index.html for handlebars
-var compileHandlebars = function() {
-  gulp.src('./src/index.hbs')
-    .pipe(handlebars())
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest('./dist'))
-    .pipe(connect.reload());
-};
+var compile = {
+  // Compile the main index.hbs > index.html for handlebars, the other templates that are required are compiled in the browserify transform
+  hbs: function() {
+    gulp.src('./src/index.hbs')
+      .pipe(handlebars())
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest('./dist'))
+      .pipe(connect.reload());
+  },
+  // Use browserify to compile the javascript and any handlebars templates that are required in the application js
+  js: function() {
+    var file = './src/js/index.js';
+    var opts = {
+      extensions: ['.hbs', '.json']
+    };
+    var stream = browserify(file, opts)
+      .transform(hbsfy)
+      .bundle()
+      .pipe(source('index.js'))
+      .on('error', utils.error)
+      .on('end', utils.end);
 
-// Use browserify to compile the javascript and any handlebars templates that are required in the application js
-var compileScripts = function() {
-  var file = './src/js/index.js';
-  var opts = {
-    extensions: ['.hbs', '.json']
-  };
-  var stream = browserify(file, opts)
-    .transform(hbsfy)
-    .bundle()
-    .pipe(source('index.js'))
-    .on('error', utils.error)
-    .on('end', utils.end);
-
-  // When the build finishes, reload connect livereload
-  stream.pipe(gulp.dest('./dist/js'))
-    .pipe(connect.reload());
-};
-
-
-var compileSass = function() {
-  gulp.src('./src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist/css'));
+    // When the build finishes, reload connect livereload
+    stream
+      .pipe(streamify(uglify()))
+      .pipe(gulp.dest('./dist/js'))
+      .pipe(connect.reload());
+  },
+  compass: function() {
+    gulp.src('./src/scss/*.scss')
+      .pipe(compass({
+        css: 'dist/css',
+        sass: 'src/scss',
+        image: 'src/images'
+      }))
+      .pipe(gulp.dest('./dist/css'));
+  }
 };
 
 // A task to run jshint on the source js files
 gulp.task('hint', function() {
-  return gulp.src('./src/**/*.js')
-    .pipe(hint(jshintOpts))
-    .pipe(hint.reporter('default'));
+  utils.hint();
 });
 
 // A separate task to compile the index.hbs to index.html
 gulp.task('handle', function() {
-  compileHandlebars();
+  compile.hbs();
 });
 
 gulp.task('sass', function() {
-  compileSass();
+  compile.compass();
 });
 // Compile the scripts and index.html
 gulp.task('build', function() {
-  compileHandlebars();
-  compileScripts();
-	compileSass();
+  compile.hbs();
+  compile.js();
+  compile.compass();
 });
 
 // Run the server and have livereload
 gulp.task('connect', function() {
-  connect.server({
-    root: './dist',
-    livereload: true,
-    port: 8888
-  });
+  utils.connect();
 });
-
 
 gulp.task('watch', function() {
   var watcher = gulp.watch('./src/**/*', ['hint', 'build', 'handle']);
